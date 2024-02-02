80d2cc27b4615e9ce2872ee627198bcd44c5483e
Adarsh Kumar Pandey <adarshpandey19101@gmail.com>
2024-02-02 06:00:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const hamburger = document.querySelector('.header .nav-bar #hamburger');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
-
 document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
 });
 
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
 
-function toggleNav() {
-    var navList = document.getElementById("nav-list");
-    navList.classList.toggle("active");
+  
+  function toggleNav() {
+    var navList = document.getElementById('navList');
+    if (navList.style.display === "flex"){
+        navList.style.display = "none";
+    } else{
+        navList.style.display = "flex";
+    }
   }
+  
+/* Existing JavaScript code */
+
+function toggleTheme() {
+    var themeToggle = document.getElementById("themeToggle");
+    var themeStylesheet = document.getElementById("themeStylesheet");
+
+    if (themeToggle.checked) {
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+    } else {
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+    }
+}
